0fb660e08f83cfe3c9e1931bda6139d03c97cc49
Daeja-BA <60225166+Daeja-BA@users.noreply.github.com>
2020-01-23 09:02:08 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".jcp").dblclick(function() {
+    $(".opink").hide();
+});
+
+
+
+$(".big-2green").hover(function() {
+    $(".jcp").hide();
+});
+
+$(".mushroom").click(function() {
+    $(".big-2green").hide();
+});
+
+$(".mario").hover(function() {
+    $(".mushroom").hide();
+});
